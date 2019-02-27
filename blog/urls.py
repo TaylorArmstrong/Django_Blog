05279b4d34a5662dcc0fe5c0781fa835b1906682
TaylorArmstrong <TArmstrong1327@gmail.com>
2019-02-26 19:27:26 -0700

    create post working

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView,
+    PostCreateView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('login/', views.login, name='blog-login'),
     path('register/', views.register, name='blog-register'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 28, 2018'
-    }
-]
 
 def home(request):
     context = {
@@ -22,6 +13,26 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
